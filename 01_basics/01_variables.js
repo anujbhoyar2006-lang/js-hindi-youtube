de0b1b748391d1a6628b87c301168a4995fb3593
Anuj <anujbhoyar2006@gmail.com>
2026-03-23 04:25:53 +0000

    DataTypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -18,4 +18,5 @@ Prefer not to use var
 beacause of issue in block scope and functional scope
 */
 
+
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs , not browser
+
+console.log(3 
+    +
+      3) //code  readability shouls be high
+
+console.log("Nandu")
+
+
+let name = "Nandu"
+let age = 41 
+let isLoggedIn = true 
+let state;
+
+// number => 2 to the power 53 
+// bigint 
+// string => ""
+// boolean => true/false 
+// null => standalone value
+// undefined => 
+// symbol => uniqueness 
+
+
+// object
+
+console.log(typeof undefined); // o/p: undefined
+console.log(typeof null); // o/p: object
