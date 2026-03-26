58f318e9668a070d70c23615cfa8076ae914b5b9
Anuj <anujbhoyar2006@gmail.com>
2026-03-26 16:16:57 +0000

    Stack and Heap memory

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -33,3 +33,31 @@ const myFunction = function(){
 }
 
 console.log(typeof anotherId);
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Memory
+// Stack (Primitive)(variable copy milta hai), Heap (Non-Primitive)(reference milta hai)
+
+let myYoutubename = "hiteshchoundharydotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
