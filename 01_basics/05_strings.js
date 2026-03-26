80c68a8433ab479b34b1a082f8c5f956b5e5a3ef
Anuj <anujbhoyar2006@gmail.com>
2026-03-26 17:07:10 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,37 @@
+const name = "Nandu"
+const repoCount = 50 
+
+// console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my rep count is ${repoCount}`);
+
+const gameName = new String('nandu-nd')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('a'));
+
+const newString = gameName.substring(0,4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-7,4)
+// console.log(anotherString);
+
+const newStringOne = "     Nandu      "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://nandu.com/nandu%20dube"
+
+// console.log(url.replace('%20','-'));
+
+// console.log(url.includes('nandu'));
+
+// console.log(url.includes('sunder'));
+
+// console.log(gameName.split('-'));
