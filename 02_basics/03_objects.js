27cd1b7eff6e9c03ba793eedc3cbe9e4d0a61911
Anuj <anujbhoyar2006@gmail.com>
2026-03-28 03:58:10 +0000

    Objects basics

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,38 @@
+// singleton - koi bhi constructor se banate hai toh singleton ek object banta hai
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name : "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "myKey1",
+    age: 18,
+    location: "jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JsUser");
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello Js User, ${this.name}`);   
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
